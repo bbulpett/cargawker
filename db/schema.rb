a6248f3a6c87b886f86e0e82b91ca3e964d9e3d3
Barnabas Bulpett <bbulpett@charter.net>
2015-06-20 16:51:12 -0400

    Conditional links for index view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607181530) do
+ActiveRecord::Schema.define(version: 20150620174605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150607181530) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "picture"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +45,5 @@ ActiveRecord::Schema.define(version: 20150607181530) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "posts", "users"
 end

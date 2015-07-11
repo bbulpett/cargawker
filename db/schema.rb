4bd07c3aa7e8faeb17385d47fc81337dc1280a6f
Barnabas Bulpett <bbulpett@charter.net>
2015-07-11 18:27:11 -0400

    Post approval and admin controls

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630235814) do
+ActiveRecord::Schema.define(version: 20150710225602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,10 +19,11 @@ ActiveRecord::Schema.define(version: 20150630235814) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "picture"
     t.integer  "user_id"
+    t.boolean  "approved",   default: false
   end
 
   create_table "users", force: :cascade do |t|

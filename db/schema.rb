d181ed74851d1277bb9b57a35ccd529f0bdf7a55
Barnabas Bulpett <bbulpett@charter.net>
2015-10-15 22:58:15 -0400

    Add link param to Posts model and whitelist it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710225602) do
+ActiveRecord::Schema.define(version: 20151016022602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150710225602) do
     t.string   "picture"
     t.integer  "user_id"
     t.boolean  "approved",   default: false
+    t.string   "link"
   end
 
   create_table "users", force: :cascade do |t|
